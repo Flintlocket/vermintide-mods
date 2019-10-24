4c406c6369ac83e8646697cf0cebdf86dd60b003
Jslon <jslom1@student.monash.edu>
2019-10-24 18:29:32 +1100

    Mod options description

--- vtgs/scripts/mods/vtgs/vtgs_localization.lua
+++ vtgs/scripts/mods/vtgs/vtgs_localization.lua
@@ -1,11 +1,11 @@
 return {
-	vtgs_keybind = {
-		en = "Open VTGS"
-	},
-	vtgs_prepend_command = {
-		en = "Prepend command to output. [VGS] SIGMAR"
-	},
-	mod_description = {
-		en = "vtgs description",
-	},
+  vtgs_keybind = {
+    en = "Open VTGS"
+  },
+  vtgs_prepend_command = {
+    en = "Prepend command to output. [VGS] SIGMAR"
+  },
+  mod_description = {
+    en = "Vermin Tide Get-better-at-communicating System - Keybind-based chat",
+  },
 }
